a75344bc0afa63992820cac9b5e1c934205fbcd4
jerryliu12 <jerryliu@umail.ucsb.edu>
2020-04-30 22:35:52 -0700

    seems to be working!

--- personalsite/src/App.js
+++ personalsite/src/App.js
@@ -4,20 +4,48 @@ import './App.css'
 import TabList from "./Components/TabList"
 import Body from "./Components/Body"
 
+function goTop(){
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 export class App extends Component {
   constructor(){
     super();
     this.state = {
-      activeTab: 0
+      activeTab: 0,
+      buttonVisible: false,
+      currScroll: document.documentElement.scrollTop
     }
     this.changeTab = (id) =>{
       this.setState({
         activeTab:id
       })
     }
+    this.handleScroll = this.handleScroll.bind(this)
   }
 
-  
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  componentWillUnmount(){
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = () => {
+    this.setState({currScroll: document.documentElement.scrollTop})
+    console.log(this.state.currScroll)
+    const canTravel = document.documentElement.scrollHeight - window.innerHeight
+    const topButton = document.getElementById("topButton");
+
+    if ( (this.state.currScroll*1.0)/canTravel >= 0.25){
+      topButton.style.display = "block";
+    }
+    else {
+      topButton.style.display = "none";
+    }
+  }
 
   render() {
     const pageName = ["Homepage", "Music", "Photos", "Miscellaneous"]
@@ -39,14 +67,15 @@ export class App extends Component {
       title: "Misc"
     }   
     ]
+
     return (
-      
       <div className="body">
         <head>
           <title>Jerry's {pageName[this.state.activeTab]}</title>
         </head>
         <div className="header">
           <h1 class="topTitle">Jerry's {pageName[this.state.activeTab]}</h1>
+          <button onClick={goTop} id='topButton'>Return to Top</button>
           {/* <button onClick={goTop} id="topButton" title="Top of Page">Return to Top</button> */}
           <div className="nav-bar space-around">
             <TabList tabs={tabs} 
